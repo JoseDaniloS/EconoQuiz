a232f9b742734427a75ecf8c76dd5a49f50d2ae1
JoseDaniloS <danilojose.1d@gmail.com>
2025-09-21 13:44:29 -0300

    feat: estrutura de pastas do Back-End e configuração inicial do servidor

---
+++ Back-End/src/middlewares/authMiddleware.js
@@ -0,0 +1,20 @@
+import { verifyToken } from "../utils/jwt.js";
+
+export function authToken(req, res, next) {
+  const authHeader = req.headers["authorization"];
+
+  const token = authHeader?.split(" ")[1];
+
+  if (!token)
+    return res
+      .status(401)
+      .json({ message: "Acesso negado. Token não fornecido." });
+
+  try {
+    const user = verifyToken(token);
+    req.user = user;
+    next();
+  } catch (err) {
+    return res.status(401).json({ message: err.message });
+  }
+}
