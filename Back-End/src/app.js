3e7ecf45e13d526af113ce45a7bb8835526b168b
JoseDaniloS <danilojose.1d@gmail.com>
2025-10-30 01:49:38 -0300

    fix: remove cors package

--- Back-End/src/app.js
+++ Back-End/src/app.js
@@ -1,47 +1,21 @@
 import express from "express";
 import dotenv from "dotenv";
 import authUser from "./routes/AuthUser.js";
-import play from "./routes/Play.js"
-import utils from "./routes/Utils.js"
-import questions from "./routes/Questions.js"
-import cors from "cors";
-
+import play from "./routes/Play.js";
+import utils from "./routes/Utils.js";
+import questions from "./routes/Questions.js";
 
 dotenv.config();
-
-const allowedOrigins = [
-  "http://localhost:5173",
-  "https://econoquiz.ufersa.dev.br",
-  "https://www.econoquiz.ufersa.dev.br",
-];
 
 export const SECRET_KEY = process.env.SECRET_KEY;
 
 const app = express();
 
-
-app.use(cors({
-    origin: (origin, callback) => {
-        if (!origin || allowedOrigins.includes(origin)) {
-            callback(null, true);
-        }
-        else {
-            callback(new Error("Origin not allowed by CORS"));
-        }
-    },
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-    credentials: true,
-}))
-
-//reponse to preflight requests
-app.options("/.*/", cors());
-
 app.use(express.json());
 
 app.use("/auth", authUser);
-app.use("/play", play)
-app.use("/questions", questions)
-app.use("/api", utils)
+app.use("/play", play);
+app.use("/questions", questions);
+app.use("/api", utils);
 
 export default app;
